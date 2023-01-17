e83567d343c10d2bbae9dd5a335d1173236d64a7
laillagaleno <laillagalenoitz@gmail.com>
2023-01-17 11:53:59 -0300

    configurações

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+app.register(cors);
+
+app.get("/hello", async ()=>{
+    const habits = await prisma.habit.findMany();
+
+    return habits;
+})
+
+app.listen({
+    port: 3000,
+})
---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
